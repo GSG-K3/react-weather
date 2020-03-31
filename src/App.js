c1f1807026fbbeb2f3fd3be2d2e5223a89b07ca4
Duha96 <141046@ppu.edu.ps>
2020-03-31 21:08:09 +0300

    location files #8

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React ,{Component}from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import SearchLocation from "./component/SearchLocation";
+class App extends Component {
+  state = {
+    lat: null,
+    log: null, 
+    cityName:"",
+    temperature:"", 
+    weather:"", 
+    icon:"",
+    wind_speed:"", 
+    Humidity:""
+  };
+  componentDidMount() {
 
-class App extends Component{
-      render(){
-      return (
-        <div className="App">
-         
-        
-        </div>
-      );
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log(position, "position");
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        this.setState=({lat:latitude,log:longitude}) 
+       
+      });
+      
+    // this.getWeather();
+    }
+    
+   
+  }   
+  render() {
+    return (
+      <div className="App">
+        <h1> WHAT'S THE WEATHER? </h1>
+        <SearchLocation city={this.state.cityName} onSubmit={this.getWeather}/>
+      </div>
+    );
   }
+ 
 }
 
 export default App;
