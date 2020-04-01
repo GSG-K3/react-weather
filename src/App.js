5697dc90b91b1d67e0eb77ff03633a4642944ed5
Duha96 <141046@ppu.edu.ps>
2020-04-01 23:56:05 +0300

    style css file for app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SearchLocation from './component/SearchLocation ';
 class App extends Component {
   state = {
+    isLoading: false,
     lat: null,
     log: null,
     city_name: '',
@@ -21,7 +22,14 @@ class App extends Component {
         console.log(position, 'position');
         let latitude = position.coords.latitude;
         let longitude = position.coords.longitude;
+        console.log(this.state.lat,"latitude");
         this.setState({ lat: latitude, log: longitude });
+       let locationAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lat}139&units=metric&apikey=cf9fb4df751879d3f30929a3dd9050e4`;
+        this.getLocation(locationAPI);
+        console.log(locationAPI,"url loction");
+        
+        
+        
       });
       
     
@@ -30,18 +38,18 @@ class App extends Component {
     }
 
   }
-  getLocation = e =>{
-    const lat =e.lat;
-      const log = e.log;
-      const locationAPI =
-      'https://api.openweathermap.org/data/2.5/weather?lat='+lat +'&lon='+log+'139&units=metric&apikey=cf9fb4df751879d3f30929a3dd9050e4';
-      fetch(locationAPI )
-      .then((res) => res.json())
-
+  getLocation = (url) =>{
+    this.setState({ isLoading: true });
+  
+      fetch(url)
       .then((res) => {
-        console.log(locationAPI)
-        this.setState({ log: console.log(res.position.coords.latitude),
-                         lat: console.log(res.position.coords.longitude) });
+        if(res.ok){
+        return  res.json()
+      }
+
+     else {
+      throw new Error("Something went wrong ...");
+    }
       })
 
       .catch((err) => console.log(err));
@@ -86,19 +94,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> WHAT'S THE WEATHER? </h1>
+        <h1 className="title"> WHAT'S THE WEATHER? </h1>
         <SearchLocation weather={this.getWeather} city={this.state.city_name} getLocation={this.getLocation} 
                              getGify={this.getGify}                                  />
           <h2>{this.state.city_name}</h2>
-            <div>
+            <div className="weaterClass">
           <h3>Temperature: {this.state.temperature}</h3>
           <h3>Weather: {this.state.weather}</h3>
           <h3>Wind speed: {this.state.wind_speed}</h3>
-            </div>
-           {/* < GetGify src={this.state.gif}/> */}
-           <img src={this.state.gif} />
-           <img src={this.state.gif1} />
-           <img src={this.state.gif2} />
+            </div >
+           <div className=' gifyClass'>
+           <img className='img' src={this.state.gif} />
+           <img className='img2'src={this.state.gif1} />
+           <img className='img3'src={this.state.gif2} />
+           </div>
       </div>
     );
     
