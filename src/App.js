9b08e9c51ad810e00d05c0f9714c0e07d973c93d
Duha96 <141046@ppu.edu.ps>
2020-04-01 12:43:45 +0300

    complet location code #10 #9

--- src/App.js
+++ src/App.js
@@ -13,16 +13,37 @@ class App extends Component {
     Humidity: ''
   };
  
-  componentDidMount() {
+  componentDidMount(e) {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
         console.log(position, 'position');
         let latitude = position.coords.latitude;
         let longitude = position.coords.longitude;
-        this.setState = { lat: latitude, log: longitude };
+        this.setState({ lat: latitude, log: longitude });
       });
-
+      
+    
+    
+    
     }
+
+  }
+  getLocation = e =>{
+    // const lat =e.lat;
+    //   const log = e.log;
+      const locationAPI =
+      'https://api.openweathermap.org/data/2.5/weather?lat='+this.lat +'&lon='+this.log+'139&units=metric&apikey=cf9fb4df751879d3f30929a3dd9050e4';
+      fetch(locationAPI )
+      .then((res) => res.json())
+
+      .then((res) => {
+        console.log(locationAPI)
+        this.setState({ log: console.log(res.position.coords.latitude),
+                         lat: console.log(res.position.coords.longitude) });
+      })
+
+      .catch((err) => console.log(err));
+    
   }
   getWeather = e => {
     const city = e.city;
@@ -35,8 +56,8 @@ class App extends Component {
         this.setState({
           city_name: response.name,
           temperature: console.log(response.main.temp),
-          weather: response.weather[0].description,
-          wind_speed: response.main.humidity
+          weather:console.log(response.weather[0].description),
+          wind_speed:console.log(response.main.humidity) 
         });
       })
   };
